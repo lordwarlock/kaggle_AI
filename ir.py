a862f92e902173fb1ff3e946be8793c0562d9c30
muyinanhai <muyinanhai@users.noreply.github.com>
2015-11-08 16:21:46 +0800

    Create ir.py
    a simple example of ir:
    1:create an index
    2:search through index

---
+++ ir.py
@@ -0,0 +1,17 @@
+#coding:utf8
+from whoosh.index import create_in
+from whoosh.fields import *
+schema =Schema(title=TEXT(stored=True),path=ID(stored=True),content=TEXT)
+ix=create_in(u"D:\\课程\\AI",schema)
+writer =ix.writer()
+writer.add_document(title=u"first document",path=u"/a",
+	content=u"This is the first document")
+writer.add_document(title=u"second document",path=u"/a",
+	content=u"This is the second document,is even more interesting")
+writer.commit()
+
+from whoosh.qparser import QueryParser
+with ix.searcher() as searcher:
+	query= QueryParser("content",ix.schema).parse("first")
+	results=searcher.search(query)
+	print results[0]
