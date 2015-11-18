59579c6a9912796efe6043a03c1d9932d92a576b
muyinanhai <muyinanhai.qq.com>
2015-11-18 11:52:23 +0800

    add dir

--- irmodule/IR.py
+++ irmodule/IR.py
@@ -1,32 +1,56 @@
 #!/usr/bin/env python
 #encoding:utf8
 from whoosh.index import create_in
+import os
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
 from FileProcess import *
+from whoosh.index import open_dir
 class IR():
-    def __init__(self,filePath,indexPath):
+    def __init__(self,filePath,indexPath,isRead=False):
         schema =Schema(title=TEXT(stored=True),path=ID(stored=True),content=TEXT(stored=True))
-        self.ix=create_in(indexPath,schema)
         self.filePath=filePath
-    def CreateIndex(self):
+        if isRead:
+            self.ix=open_dir(indexPath)
+        else:
+            self.ix=create_in(indexPath,schema)
+            self.addIndex(filePath)
+
+    def CreateIndex(self,data):
         writer =self.ix.writer()
-        data=readWikipedia(self.filePath)
         for item in data:
             writer.add_document(title=item['title'].decode("utf-8"),path=u"/a",content=item["content"].decode("utf-8"))
         writer.commit()
+
+    def dirlist(self,path, allfile):
+        if os.path.isfile(path):
+            allfile.append(path)
+            return allfile
+	filelist =  os.listdir(path)
+        for filename in filelist:
+            filepath = os.path.join(path, filename)
+            if os.path.isdir(filepath):
+                dirlist(filepath, allfile)
+            else:
+                allfile.append(filepath)
+        return allfile
+
+    def addIndex(self,filePath):
+        allfile=self.dirlist(filePath,[])
+        for efile in allfile:
+            print efile
+            self.CreateIndex(readWikipedia(efile))
         print "index finished"
-    
+
     def Search(self,query):
         with self.ix.searcher() as searcher:
             query= QueryParser("content",self.ix.schema).parse(query)
 	    results=searcher.search(query)
-            print results[0]
-            return results
+            print results[0]["title"]
+            #return results
 
 
 if __name__=="__main__":
     print "start"
-    ir=IR("top1000.xml","./index/")
-    ir.CreateIndex()
+    ir=IR("data","./index/",False)
     ir.Search(u"autistic symptoms")
